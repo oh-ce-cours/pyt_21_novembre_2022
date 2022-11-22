ada7053ca467a4c544c9be7aa339a8e96f165a0b
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-22 12:01:38 +0100

    gitautosync update 177

--- tape_en_cours/plus_moins.py
+++ tape_en_cours/plus_moins.py
@@ -4,12 +4,14 @@ Un jeu super !
 NOMBRE_A_TROUVER = 10
 mon_entree = int(input("Entrez un nombre : "))
 
-while NOMBRE_A_TROUVER != mon_entree:
+while True:
     if NOMBRE_A_TROUVER > mon_entree:
         print("c'est plus")
     elif NOMBRE_A_TROUVER < mon_entree:
         print("c'est moins")
-    else:
+    elif NOMBRE_A_TROUVER < mon_entree:
         print("c'est ça")
+        break
+    else:
     mon_entree = int(input("Entrez un nombre : "))
 print("Gagné")
