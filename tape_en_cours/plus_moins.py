766a87b499e1ab6f486ec6d9d7c5577313a71a04
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-22 12:12:03 +0100

    gitautosync update 194

--- tape_en_cours/plus_moins.py
+++ tape_en_cours/plus_moins.py
@@ -8,14 +8,15 @@ while True:
         mon_entree = int(input("Entrez un nombre : "))
     except ValueError:
         print("On a dit un nombre, svp")
+        continue
+
+    if NOMBRE_A_TROUVER > mon_entree:
+        print("c'est plus")
+    elif NOMBRE_A_TROUVER < mon_entree:
+        print("c'est moins")
     else:
-        if NOMBRE_A_TROUVER > mon_entree:
-            print("c'est plus")
-        elif NOMBRE_A_TROUVER < mon_entree:
-            print("c'est moins")
-        else:
-            print("c'est ça")
-            break
+        print("c'est ça")
+        break
 
 
 print("Gagné")
