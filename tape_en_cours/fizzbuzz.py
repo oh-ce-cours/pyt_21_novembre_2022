2625b37d2f8a26fd22d3307e5b91936e9514b858
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-22 12:31:40 +0100

    gitautosync update 219

--- tape_en_cours/fizzbuzz.py
+++ tape_en_cours/fizzbuzz.py
@@ -14,7 +14,7 @@ import time
 
 
 def est_divisible_par(nombre_a_diviser, diviseur):
-    nombre_a_diviser % diviseur == 0
+    res = nombre_a_diviser % diviseur == 0
     # return nombre_a_diviser // diviseur == nombre_a_diviser / diviseur
 
 
