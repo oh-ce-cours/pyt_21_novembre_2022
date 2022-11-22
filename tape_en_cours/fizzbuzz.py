35096d92adeab7e2f6ec72cc057f77cfb3a5ecbe
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-22 10:17:14 +0100

    gitautosync update 122

--- tape_en_cours/fizzbuzz.py
+++ tape_en_cours/fizzbuzz.py
@@ -8,12 +8,23 @@
 #     else:
 #         print(nombre)
 
+# for nombre in range(1, 101):
+#     res = ""
+#     if nombre % 3 == 0:
+#         res += "fizz"
+#     if nombre % 5 == 0:
+#         res += "buzz"
+#     if not res:
+#         res = str(nombre)
+#     print(res)
+
 for nombre in range(1, 101):
-    res = ""
-    if nombre % 3 == 0:
-        res += "fizz"
-    if nombre % 5 == 0:
-        res += "buzz"
-    if not res:
-        res = str(nombre)
-    print(res)
+    match nombre % 3, nombre %5:
+        case pattern-1:
+            action-1
+        case pattern-2:
+            action-2
+        case pattern-3:
+            action-3
+        case _:
+            action-default
