d6801db72d27d7de8f43622295a9684b363e12a9
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-22 12:19:00 +0100

    gitautosync update 197

--- tape_en_cours/plus_moins.py
+++ tape_en_cours/plus_moins.py
@@ -5,12 +5,16 @@ import random
 
 NOMBRE_A_TROUVER = random.randint(0, 100)
 
-while True:
+
+def mon_input() -> int:
     try:
         mon_entree = int(input("Entrez un nombre : "))
     except ValueError:
         print("On a dit un nombre, svp")
         continue
+
+
+while True:
 
     if NOMBRE_A_TROUVER > mon_entree:
         print("c'est plus")
