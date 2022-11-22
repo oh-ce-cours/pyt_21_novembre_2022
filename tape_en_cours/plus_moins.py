94f1a2d277c6a3f05e8443fd0bba17a5bd5853aa
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-22 12:10:09 +0100

    gitautosync update 192

--- tape_en_cours/plus_moins.py
+++ tape_en_cours/plus_moins.py
@@ -6,13 +6,15 @@ NOMBRE_A_TROUVER = 10
 while True:
     try:
         mon_entree = int(input("Entrez un nombre : "))
-        if NOMBRE_A_TROUVER > mon_entree:
-            print("c'est plus")
-        elif NOMBRE_A_TROUVER < mon_entree:
-            print("c'est moins")
-        else:
-            print("c'est ça")
-            break
     except ValueError:
         print("On a dit un nombre, svp")
+    if NOMBRE_A_TROUVER > mon_entree:
+        print("c'est plus")
+    elif NOMBRE_A_TROUVER < mon_entree:
+        print("c'est moins")
+    else:
+        print("c'est ça")
+        break
+
+
 print("Gagné")
