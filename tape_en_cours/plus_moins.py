7d61cad4adea118f158bc3de0d1c4be4d902faa0
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-22 12:03:28 +0100

    gitautosync update 182

--- tape_en_cours/plus_moins.py
+++ tape_en_cours/plus_moins.py
@@ -4,7 +4,6 @@ Un jeu super !
 NOMBRE_A_TROUVER = 10
 
 while mon_entree := int(input("Entrez un nombre : ")):
-
     if NOMBRE_A_TROUVER > mon_entree:
         print("c'est plus")
     elif NOMBRE_A_TROUVER < mon_entree:
