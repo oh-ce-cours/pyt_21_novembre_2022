2886c40bdb40296ab04e8f6af77d24d23369e585
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-23 12:50:47 +0100

    gitautosync update 90

--- tape_en_cours/unix.py
+++ tape_en_cours/unix.py
@@ -47,7 +47,7 @@ def head_lazy_1():
 
 def head_lazy_2():
     with open("./plus_moins.py", encoding="utf8") as f:
-        for line_number, line in zip(f):
+        for line_number, line in zip(f, range(5)):
             if line_number > 5:
                 break
             print(line.rstrip())
