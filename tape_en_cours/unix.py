1bd20226c8e1692697b13ae3f461be2d715ce0e6
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-23 10:33:02 +0100

    gitautosync update 16

--- tape_en_cours/unix.py
+++ tape_en_cours/unix.py
@@ -4,5 +4,10 @@ def cat():
         for line in lines:
             print(line.rstrip())
 
+def tac():
+    with open("./plus_moins.py", encoding="utf8") as f:
+        lines = f.readlines()
+        for line in lines:
+            print(line.rstrip())
 
 cat()
