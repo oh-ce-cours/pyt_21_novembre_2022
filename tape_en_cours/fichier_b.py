bd20b4f7c41967ae46ba0810cb68ac89a3c2a9f6
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-23 15:20:40 +0100

    gitautosync update 216

--- tape_en_cours/fichier_b.py
+++ tape_en_cours/fichier_b.py
@@ -1,10 +1,16 @@
 from fichier_a import f
+
 print("f original", id(f))
+
 
 def f():
     pass
 
 
+print("f copie", id(f))
+from fichier_a import f
+
+print("f copie", id(f))
 
 print("dans fichier b", f"{__name__=}")
 fichier_a.f()
