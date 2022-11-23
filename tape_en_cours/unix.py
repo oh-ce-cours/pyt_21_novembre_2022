b3ba8eb6223238238aa0d46fa9d52c99efa0df46
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-23 13:02:55 +0100

    gitautosync update 118

--- tape_en_cours/unix.py
+++ tape_en_cours/unix.py
@@ -87,7 +87,9 @@ def tail_lazy():
 
 def tail_lazy_2():
     with open("./plus_moins.py", encoding="utf8") as f:
-        
+        q = deque(maxlen=10)
+        for line in f:
+            
         return deque(f, 10)
 
 
