50a3bd05aa057ce205d9959fa7fa072b68bf67d7
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-23 12:15:20 +0100

    gitautosync update 65

--- tape_en_cours/unix.py
+++ tape_en_cours/unix.py
@@ -27,6 +27,12 @@ def head():
         for line in lines[:5]:
             print(line.rstrip())
 
+def head_laszy_1():
+    with open("./plus_moins.py", encoding="utf8") as f:
+        lines = f.readlines()
+        for line in lines[:5]:
+            print(line.rstrip())
+
 def head():
     with open("./plus_moins.py", encoding="utf8") as f:
         lines = f.readlines()
