e89733ba0a58d98a55923a90087be366064aa3a2
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-24 12:08:33 +0100

    gitautosync update 307

--- tape_en_cours/demo_scope_fonction.py
+++ tape_en_cours/demo_scope_fonction.py
@@ -1,14 +1,16 @@
 variable = 1
 
+def example():
+    
 
-def modifie_variable_ok():
-    global variable
-    variable += 1
+# def modifie_variable_ok():
+#     global variable
+#     variable += 1
 
 
-def modifie_variable_ok2():
-    # global variable
-    variable += 1
+# def modifie_variable_ok2():
+#     # global variable
+#     variable += 1
 
 
 modifie_variable_ok()
