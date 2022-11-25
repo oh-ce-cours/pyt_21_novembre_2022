21384ba6cb7712dff003ec4c05bc7c8b36c4120d
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-25 10:32:16 +0100

    gitautosync update 419

--- tape_en_cours/personne_formation.py
+++ tape_en_cours/personne_formation.py
@@ -1,2 +1,9 @@
 class Personne:
-    def __init__(self, )
+    AGE_MAJORITE = 18
+
+    def __init__(self, naissance, nom):
+        self.naissance = naissance
+        self.nom = nom
+
+    def est_majeur(self):
+        return (2022 - self.naissance) > self.AGE_MAJORITE
