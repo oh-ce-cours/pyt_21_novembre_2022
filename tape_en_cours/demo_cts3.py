307c92daeda8381814441e5ed2d3d6f33b4a8ce9
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-25 11:34:47 +0100

    gitautosync update 519

--- tape_en_cours/demo_cts3.py
+++ tape_en_cours/demo_cts3.py
@@ -12,16 +12,16 @@ class CTS3:
             blocking_mode=ni_cts3.ResourceBlockingMode.OVERRIDE,
         )
         ni_cts3.MPOS_OpenResource(
-            ni_cts3.ResourceType.CTS3_NFC_RESOURCE_ID,
+            ni_cts3.ResourceType.CTS3_DAQ_RESOURCE_ID,
             blocking_mode=ni_cts3.ResourceBlockingMode.OVERRIDE,
         )
 
 
-ni_cts3.OpenCommunication("192.168.142.1", log=True)
-ni_cts3.MPOS_OpenResource(
-    ni_cts3.ResourceType.CTS3_NFC_RESOURCE_ID,
-    blocking_mode=ni_cts3.ResourceBlockingMode.OVERRIDE,
-)
+# ni_cts3.OpenCommunication("192.168.142.1", log=True)
+# ni_cts3.MPOS_OpenResource(
+#     ni_cts3.ResourceType.CTS3_NFC_RESOURCE_ID,
+#     blocking_mode=ni_cts3.ResourceBlockingMode.OVERRIDE,
+# )
 Nfc.MPC_SelectFieldStrength(Nfc.FieldUnit.UNIT_MV_RANGE_25V, 10_000)
 time.sleep(5)
 ni_cts3.CloseCommunication()
