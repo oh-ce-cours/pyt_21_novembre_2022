c64ec0d1566e4949e758f1e9085fc5527b7c1f49
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-25 11:44:41 +0100

    gitautosync update 544

--- tape_en_cours/demo_cts3.py
+++ tape_en_cours/demo_cts3.py
@@ -21,7 +21,8 @@ class CTS3:
     def champ(self, puissance):
         pass
 
-    @champ.property
+    @champ.setter
+    def champ(self, puissance)
         Nfc.MPC_SelectFieldStrength(Nfc.FieldUnit.UNIT_MV_RANGE_25V, puissance)
 
     def close(self):
