49c39b40b73e0477cc3e266759c3b99fb9e89918
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-25 14:24:25 +0100

    gitautosync update 683

--- tape_en_cours/send_email.py
+++ tape_en_cours/send_email.py
@@ -11,8 +11,4 @@ This message is sent from Python."""
 
 context = ssl.create_default_context()
 with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo()  # Can be omitted
-    server.starttls(context=context)
-    server.ehlo()  # Can be omitted
-    server.login(sender_email, "")
     server.sendmail(sender_email, receiver_email, message)
