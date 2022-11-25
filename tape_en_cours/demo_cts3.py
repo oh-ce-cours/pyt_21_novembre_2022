db8db99d605c34fce146c0d5fb6434838a25be96
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-25 12:02:58 +0100

    gitautosync update 594

--- tape_en_cours/demo_cts3.py
+++ tape_en_cours/demo_cts3.py
@@ -10,13 +10,13 @@ class CTS3:
         self.log_file = Path(__file__).with_suffix(".log")
         print(self.log_file)
         ni_cts3.OpenCommunication(ip, log=log)
-        self.new_method()
+        self.set_NFC()
         self.set_dac()
         ni_cts3.SetDLLDebugMode(self.log_file)
         # ni_cts3.MPS_Beep(0.1)
         self.__puissance = None
 
-    def new_method(self):
+    def set_NFC(self):
         ni_cts3.MPOS_OpenResource(
             ni_cts3.ResourceType.CTS3_NFC_RESOURCE_ID,
             blocking_mode=ni_cts3.ResourceBlockingMode.OVERRIDE,
