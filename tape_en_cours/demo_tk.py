29ae2d77137a88c358c4c05339c32ddebeceff66
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-25 15:11:51 +0100

    gitautosync update 725

--- tape_en_cours/demo_tk.py
+++ tape_en_cours/demo_tk.py
@@ -3,21 +3,13 @@ from tkinter import ttk
 
 # root window
 root = tk.Tk()
-root.geometry('300x200')
+root.geometry("300x200")
 root.resizable(False, False)
-root.title('Button Demo')
+root.title("Button Demo")
 
 # exit button
-exit_button = ttk.Button(
-    root,
-    text='Click me :)',
-    command=lambda: root.quit()
-)
+exit_button = ttk.Button(root, text="Click me :)", command=... )
 
-exit_button.pack(
-    ipadx=5,
-    ipady=5,
-    expand=True
-)
+exit_button.pack(ipadx=5, ipady=5, expand=True)
 
 root.mainloop()
