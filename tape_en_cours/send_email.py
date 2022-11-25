0346c3f5809099f634699de6b2a7aa693d0288ca
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-25 14:40:28 +0100

    gitautosync update 705

--- tape_en_cours/send_email.py
+++ tape_en_cours/send_email.py
@@ -1,6 +1,5 @@
 import smtplib, ssl
 from os.path import basename
-from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 
