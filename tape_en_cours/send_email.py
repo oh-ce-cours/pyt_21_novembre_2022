2fae1e4fc84d351cc4a508df79a18dfd2ddf2687
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-25 14:36:19 +0100

    gitautosync update 695

--- tape_en_cours/send_email.py
+++ tape_en_cours/send_email.py
@@ -7,13 +7,20 @@ from email.mime.application import MIMEApplication
 
 port = 587  # For starttls
 smtp_server = "smtp.gmail.com"
-sender_email = "falce.matthieu@gmail.com"
-receiver_email = "matthieu@falce.net"
-password = "lthumuxpgbezukem"
-message = """\
-Subject: Hi there
 
-This message is sent from Python."""
+
+sender = "admin@example.com"
+receiver = "info@example.com"
+
+msg = MIMEMultipart()
+msg["Subject"] = "Test mail with attachment"
+msg["From"] = "admin@example.com"
+msg["To"] = "info@example.com"
+
+filename = "words.txt"
+with open(filename, "r") as f:
+    part = MIMEApplication(f.read(), Name=basename(filename))
+
 
 context = ssl.create_default_context()
 with smtplib.SMTP(smtp_server, port) as server:
