ea8ca462d7b634dfc7dbc5990717a2efacbee6c0
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-25 11:31:35 +0100

    gitautosync update 508

--- tape_en_cours/demo_cts3.py
+++ tape_en_cours/demo_cts3.py
@@ -2,6 +2,9 @@ import ni_cts3
 from ni_cts3 import Nfc
 
 ni_cts3.OpenCommunication("192.168.142.1", log=True)
-ni_cts3.MPOS_OpenResource(ni_cts3.ResourceType.CTS3_NFC_RESOURCE_ID, blocking_mode=)
+ni_cts3.MPOS_OpenResource(
+    ni_cts3.ResourceType.CTS3_NFC_RESOURCE_ID,
+    blocking_mode=ni_cts3.ResourceBlockingMode.OVERRIDE,
+)
 Nfc.MPC_SelectFieldStrength(Nfc.FieldUnit.UNIT_MV_RANGE_25V, 10_000)
 ni_cts3.CloseCommunication()
