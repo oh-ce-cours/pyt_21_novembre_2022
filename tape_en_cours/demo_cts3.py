83d4876d07e2f99e0f47fc1102626d61bb65075f
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-25 11:41:29 +0100

    gitautosync update 533

--- tape_en_cours/demo_cts3.py
+++ tape_en_cours/demo_cts3.py
@@ -17,7 +17,7 @@ class CTS3:
         )
         ni_cts3.MPS_Beep(0.1)
 
-    def activer_champ(self):
+    def activer_champ(self, puissance):
         Nfc.MPC_SelectFieldStrength(Nfc.FieldUnit.UNIT_MV_RANGE_25V, 10_000)
 
     def close(self):
