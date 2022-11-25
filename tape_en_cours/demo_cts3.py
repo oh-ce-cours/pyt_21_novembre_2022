6c2ca3d11167064874d47c70d7b6e9bf3a6c3ac6
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-25 12:01:46 +0100

    gitautosync update 588

--- tape_en_cours/demo_cts3.py
+++ tape_en_cours/demo_cts3.py
@@ -24,7 +24,7 @@ class CTS3:
         self.set_dac()
 
     def set_dac(self):
-        Daq_SetChannel(ACQ_CHANNEL, True, DaqRange.RANGE_2000)
+        ni_cts3.Daq_SetChannel(ACQ_CHANNEL, True, DaqRange.RANGE_2000)
         Daq_SetTimeBase(DaqSamplingClk.SCLK_150MHZ, points_number)
         Daq_SetTrigger(TRIGGER, THRESHOLD, DaqTrigDir.DIR_RISING_EDGE, 0)
 
