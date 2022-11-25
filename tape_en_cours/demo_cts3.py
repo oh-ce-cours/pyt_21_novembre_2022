2df51b0e58b0d6235881bdd7f1e11f4714b93bb9
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-25 11:31:57 +0100

    gitautosync update 509

--- tape_en_cours/demo_cts3.py
+++ tape_en_cours/demo_cts3.py
@@ -7,4 +7,5 @@ ni_cts3.MPOS_OpenResource(
     blocking_mode=ni_cts3.ResourceBlockingMode.OVERRIDE,
 )
 Nfc.MPC_SelectFieldStrength(Nfc.FieldUnit.UNIT_MV_RANGE_25V, 10_000)
+
 ni_cts3.CloseCommunication()
