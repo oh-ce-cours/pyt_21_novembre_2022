6b6387c371516dbeebde44e7592479e5c8e09e80
Matthieu Falce <falce.matthieu@gmail.com>
2022-11-25 11:21:52 +0100

    gitautosync update 492

--- tape_en_cours/demo_cts3.py
+++ tape_en_cours/demo_cts3.py
@@ -2,4 +2,5 @@ import ni_cts3
 from ni_cts3 import Nfc
 
 ni_cts3.OpenCommunication("192.168.142.1", log=True)
-Nfc.MPC_SelectFieldStrength(Nfc.FieldUnit.UNIT_MV_RANGE_25V, 10_000)
+error_code = Nfc.MPC_SelectFieldStrength(Nfc.FieldUnit.UNIT_MV_RANGE_25V, 10_000)
+print(error_code)
